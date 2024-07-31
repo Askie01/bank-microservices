cc0d2cdd55ac23793fc7476631e58a9e69c03637
Askie01 <adam.paw01@gmail.com>
2024-08-01 01:20:33 +0200

    Create account & customer repositories.
    Created mentioned repositories.

---
+++ src/main/java/org/askie01/accounts/repositories/AccountRepository.java
@@ -0,0 +1,9 @@
+package org.askie01.accounts.repositories;
+
+import org.askie01.accounts.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
---
+++ src/main/java/org/askie01/accounts/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package org.askie01.accounts.repositories;
+
+import org.askie01.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
