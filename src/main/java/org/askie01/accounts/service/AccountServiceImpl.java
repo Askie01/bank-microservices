716a1c76f2e7595d06923a5ae2380cc588825327
Askie01 <adam.paw01@gmail.com>
2024-08-01 17:54:27 +0200

    Partially update 'AccountServiceImpl'.
    Partially updated mentioned service - this still requires further implementation.
    Commit is made to keep changes on PC.

--- src/main/java/org/askie01/accounts/service/AccountServiceImpl.java
+++ src/main/java/org/askie01/accounts/service/AccountServiceImpl.java
@@ -1,10 +1,16 @@
 package org.askie01.accounts.service;
 
 import lombok.Data;
+import org.askie01.accounts.constant.AccountConstants;
 import org.askie01.accounts.dto.CustomerDTO;
+import org.askie01.accounts.entity.Account;
+import org.askie01.accounts.entity.Customer;
+import org.askie01.accounts.mapper.CustomerMapper;
 import org.askie01.accounts.repositories.AccountRepository;
 import org.askie01.accounts.repositories.CustomerRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Data
 @Service
@@ -15,6 +21,19 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void createAccount(CustomerDTO customerDTO) {
+        final Customer customer = CustomerMapper.mapToCustomer(customerDTO, new Customer());
+        Customer savedCustomer = customerRepository.save(customer);
+        accountRepository.save(createAccount(savedCustomer));
+    }
 
+    private Account createAccount(Customer customer) {
+        final Account account = new Account();
+        account.setCustomerId(customer.getCustomerId());
+        final long randomAccountNumber = 1000000000L + new Random().nextInt(900000000);
+
+        account.setAccountNumber(randomAccountNumber);
+        account.setAccountType(AccountConstants.SAVINGS);
+        account.setBranchAddress(AccountConstants.ADDRESS);
+        return account;
     }
 }
