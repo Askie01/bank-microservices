989e1ffc2f62df9e361e532f2ffcab396b8bc445
Askie01 <adam.paw01@gmail.com>
2024-08-01 20:21:32 +0200

    Create 'ResourceNotFoundException' & update 'GlobalExceptionHandler'.
    Created new exception and it's handling in GlobalHandler.

--- src/main/java/org/askie01/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/org/askie01/accounts/exception/GlobalExceptionHandler.java
@@ -21,4 +21,15 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now());
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.NOT_FOUND,
+                exception.getMessage(),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
+    }
 }
