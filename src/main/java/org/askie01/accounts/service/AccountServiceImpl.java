348b091b163ee58fd8042dcc6edbce1a942749c0
Askie01 <adam.paw01@gmail.com>
2024-08-01 19:15:40 +0200

    Update 'AccountServiceImpl'.
    Now we're able to check if mobile phone already exists - if it does, throw an exception, if it's not - create, save & return account.

--- src/main/java/org/askie01/accounts/service/AccountServiceImpl.java
+++ src/main/java/org/askie01/accounts/service/AccountServiceImpl.java
@@ -5,12 +5,14 @@ import org.askie01.accounts.constant.AccountConstants;
 import org.askie01.accounts.dto.CustomerDTO;
 import org.askie01.accounts.entity.Account;
 import org.askie01.accounts.entity.Customer;
+import org.askie01.accounts.exception.MobilePhoneAlreadyExistsException;
 import org.askie01.accounts.mapper.CustomerMapper;
 import org.askie01.accounts.repositories.AccountRepository;
 import org.askie01.accounts.repositories.CustomerRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Data
 @Service
@@ -22,18 +24,23 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void createAccount(CustomerDTO customerDTO) {
         final Customer customer = CustomerMapper.mapToCustomer(customerDTO, new Customer());
-        Customer savedCustomer = customerRepository.save(customer);
+        final Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customerDTO.getMobileNumber());
+        if (optionalCustomer.isPresent()) {
+            throw new MobilePhoneAlreadyExistsException("Mobile phone: '" + customerDTO.getMobileNumber() + "' already exists");
+        }
+        customer.setCreatedAt(LocalDateTime.now());
+        customer.setCreatedBy("Anonymous");
+        final Customer savedCustomer = customerRepository.save(customer);
         accountRepository.save(createAccount(savedCustomer));
     }
 
     private Account createAccount(Customer customer) {
         final Account account = new Account();
-        account.setCustomerId(customer.getCustomerId());
-        final long randomAccountNumber = 1000000000L + new Random().nextInt(900000000);
-
-        account.setAccountId(randomAccountNumber);
+        account.setCustomer(customer);
         account.setAccountType(AccountConstants.SAVINGS);
         account.setBranchAddress(AccountConstants.ADDRESS);
+        account.setCreatedAt(LocalDateTime.now());
+        account.setCreatedBy("Anonymous");
         return account;
     }
 }
