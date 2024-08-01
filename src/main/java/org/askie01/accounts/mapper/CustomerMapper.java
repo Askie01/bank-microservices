7b11085142deff805eaa97722c9948bd57d56056
Askie01 <adam.paw01@gmail.com>
2024-08-01 16:20:30 +0200

    Create account & customer mappers.
    Created mentioned mappers to transfer data between 'Account' & 'AccountDTO' objects - same for 'Customer' & 'CustomerDTO'.

---
+++ src/main/java/org/askie01/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,20 @@
+package org.askie01.accounts.mapper;
+
+import org.askie01.accounts.dto.CustomerDTO;
+import org.askie01.accounts.entity.Customer;
+
+public class CustomerMapper {
+    public static CustomerDTO mapToCustomerDTO(Customer customer, CustomerDTO customerDTO) {
+        customerDTO.setName(customer.getName());
+        customerDTO.setEmail(customer.getEmail());
+        customerDTO.setMobileNumber(customer.getMobileNumber());
+        return customerDTO;
+    }
+
+    public static Customer mapToCustomer(CustomerDTO customerDTO, Customer customer) {
+        customer.setName(customerDTO.getName());
+        customer.setEmail(customerDTO.getEmail());
+        customer.setMobileNumber(customerDTO.getMobileNumber());
+        return customer;
+    }
+}
