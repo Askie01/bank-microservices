e1675d53f27d4f58ca7a74a2a342440190c09127
Askie01 <adam.paw01@gmail.com>
2024-08-07 09:56:31 +0200

    Update 'GlobalExceptionHandler'.
    * Created exception handling for 'Exception.class'.

--- src/main/java/org/askie01/accounts/exception/GlobalExceptionHandler.java
+++ src/main/java/org/askie01/accounts/exception/GlobalExceptionHandler.java
@@ -12,9 +12,20 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleGlobalException(Exception exception, WebRequest webRequest) {
+        final ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(MobilePhoneAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDTO> handleMobilePhoneAlreadyExistsException(MobilePhoneAlreadyExistsException exception, WebRequest webRequest) {
-        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
+        final ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
                 webRequest.getDescription(false),
                 HttpStatus.BAD_REQUEST,
                 exception.getMessage(),
@@ -24,7 +35,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest) {
-        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
+        final ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
                 webRequest.getDescription(false),
                 HttpStatus.NOT_FOUND,
                 exception.getMessage(),
