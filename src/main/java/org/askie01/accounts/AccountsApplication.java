8eff26efd1db9793e13a61e09198f6103baf1206
Askie01 <adam.paw01@gmail.com>
2024-08-10 01:39:41 +0200

    Create small REST API documentation.
    * Created a small REST API documentation with the help of new added dependency to document my API.
    * However this still need more documentation and feature to consider it 'done'.

--- src/main/java/org/askie01/accounts/AccountsApplication.java
+++ src/main/java/org/askie01/accounts/AccountsApplication.java
@@ -1,11 +1,31 @@
 package org.askie01.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "Askie01 Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Adam Pawłowski"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        ), externalDocs = @ExternalDocumentation(
+        description = "Askie01 Accounts microservice REST API Documentation"
+)
+)
 public class AccountsApplication {
 
     public static void main(String[] args) {
