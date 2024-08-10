8250368229ccc1c9a8939e8a1bf8e40a2dd0af82
Askie01 <adam.paw01@gmail.com>
2024-08-10 13:19:43 +0200

    Create loan microservice & documentation.

--- loans/src/main/java/com/askie01/loans/LoansApplication.java
+++ loans/src/main/java/com/askie01/loans/LoansApplication.java
@@ -1,9 +1,28 @@
 package com.askie01.loans;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "Askie loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Askie01"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        )
+)
 public class LoansApplication {
 
     public static void main(String[] args) {
