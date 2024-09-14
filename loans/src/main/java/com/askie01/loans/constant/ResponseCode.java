7cbf376681473da60eccc36ce99019e048e12128
Askie01 <adam.paw01@gmail.com>
2024-09-14 10:56:11 +0200

    Update 'ResponseCode' & 'ResponseMessage' in 'Loans' microservice.
    * Updated 'ResponseCode' class with additional fields.
    * Fixed typo in 'CREATED' field of 'ResponseMessage' class. Updated messages in other fields as well.

--- loans/src/main/java/com/askie01/loans/constant/ResponseCode.java
+++ loans/src/main/java/com/askie01/loans/constant/ResponseCode.java
@@ -5,8 +5,10 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ResponseCode {
-    public static final String OK = "200";
-    public static final String CREATED = "201";
-    public static final String EXPECTATION_FAILED = "417";
-    public static final String SERVER_ERROR = "500";
+    public static final Integer OK = 200;
+    public static final Integer CREATED = 201;
+    public static final Integer BAD_REQUEST = 400;
+    public static final Integer NOT_FOUND = 404;
+    public static final Integer EXPECTATION_FAILED = 417;
+    public static final Integer INTERVAL_SERVER_ERROR = 500;
 }
--- loans/src/main/java/com/askie01/loans/constant/ResponseMessage.java
+++ loans/src/main/java/com/askie01/loans/constant/ResponseMessage.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ResponseMessage {
     public static final String OK = "Request processed successfully";
-    public static final String CREATED = "Card created successfully";
-    public static final String EXPECTATION_FAILED = "Delete operation failed. Please try again or contact Dev team";
-    public static final String SERVER_ERROR = "An error occurred. Please try again or contact Dev team";
+    public static final String CREATED = "Loan created successfully";
+    public static final String EXPECTATION_FAILED = "Delete operation failed.";
+    public static final String SERVER_ERROR = "Server error occurred.";
 }
