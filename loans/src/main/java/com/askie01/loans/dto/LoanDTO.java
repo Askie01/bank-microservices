173711ba814024ccdb80fd2984500922370ed854
Askie01 <adam.paw01@gmail.com>
2024-09-14 11:01:57 +0200

    Refactor 'LoanDTO'.
    * Removed annotation-based documentation & changed field names to match fields in 'Loan' entity.
    * Changed this class to match its purpose - to use this object to only display data, not also handle creation/update requests.

--- loans/src/main/java/com/askie01/loans/dto/LoanDTO.java
+++ loans/src/main/java/com/askie01/loans/dto/LoanDTO.java
@@ -1,31 +1,13 @@
 package com.askie01.loans.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class LoanDTO {
-
-    @NotEmpty(message = "Mobile number cannot be a null or empty")
-    @Pattern(regexp = "(^$|[0-9]{9})", message = "Mobile number must be a 9 digits")
-    private String mobileNumber;
-
-    @NotEmpty(message = "Loan number cannot be a null or empty")
-    @Pattern(regexp = "(^$|[0-9]+)", message = "Loan number can only contain digits")
-    private String loanNumber;
-
-    @NotEmpty(message = "Loan type cannot be a null or empty")
-    private String loanType;
-
-    @Positive(message = "Total loan amount should be greater than zero")
-    private int totalLoan;
-
-    @PositiveOrZero(message = "Total loan amount paid should be equal or greater than zero")
-    private int amountPaid;
-
-    @PositiveOrZero(message = "Total outstanding amount should be equal or greater than zero")
-    private int outstandingAmount;
+    private Long number;
+    private String type;
+    private Integer mobileNumber;
+    private Integer moneyLoaned;
+    private Integer moneyPaid;
+    private Integer moneyRemaining;
 }
