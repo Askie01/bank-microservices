360aecb5962b31341cbc1dadd57c9a7f627f6803
Askie01 <adam.paw01@gmail.com>
2024-09-14 11:07:03 +0200

    Update 'ResourceNotFoundException' in 'Loans' microservice.
    * Added 2 more constructor for different fieldValue types - to remove the need of adding '.toString()' method on all non-string fields.

--- loans/src/main/java/com/askie01/loans/exception/ResourceNotFoundException.java
+++ loans/src/main/java/com/askie01/loans/exception/ResourceNotFoundException.java
@@ -5,6 +5,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue) {
+        super(String.format("'%s' not found with the given input data '%s' : '%d'", resourceName, fieldName, fieldValue));
+    }
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Integer fieldValue) {
+        super(String.format("'%s' not found with the given input data '%s' : '%d'", resourceName, fieldName, fieldValue));
+    }
+
     public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
         super(String.format("'%s' not found with the given input data '%s' : '%s'", resourceName, fieldName, fieldValue));
     }
