232887241b14c281856f1e5a97ef1a1977dba60e
Askie01 <adam.paw01@gmail.com>
2024-09-18 19:17:57 +0200

    Fixup 'id' in 'LoanTypeRepository'.
    * Fixed 'id' property in 'LoanTypeRepository' to Long, instead of Integer - due to 'LoanType' 'id' field type.

--- loans/src/main/java/com/askie01/loans/repository/LoanTypeRepository.java
+++ loans/src/main/java/com/askie01/loans/repository/LoanTypeRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface LoanTypeRepository extends JpaRepository<LoanType, Integer> {
+public interface LoanTypeRepository extends JpaRepository<LoanType, Long> {
     Optional<LoanType> findByName(String name);
 }
