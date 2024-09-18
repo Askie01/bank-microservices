9f515f7ba64ff3ded8c710603196604ff7305f59
Askie01 <adam.paw01@gmail.com>
2024-09-18 19:03:33 +0200

    Remove lombok in 'LoanType'.
    * Removed redundant lombok annotations in 'LoanType'.

--- loans/src/main/java/com/askie01/loans/entity/LoanType.java
+++ loans/src/main/java/com/askie01/loans/entity/LoanType.java
@@ -2,13 +2,7 @@ package com.askie01.loans.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
-@Data
-@ToString(callSuper = true)
-@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "loan_types")
 public class LoanType extends NamedEntity {
