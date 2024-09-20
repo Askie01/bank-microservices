77c7e2383ec11ff224155d79fd10e6d4035d8b4d
Askie01 <adam.paw01@gmail.com>
2024-09-20 17:32:29 +0200

    Update 'ResponseCode' & 'ResponseMessage'.
    * Added new codes & messages.

--- accounts/src/main/java/org/askie01/accounts/constant/ResponseMessage.java
+++ accounts/src/main/java/org/askie01/accounts/constant/ResponseMessage.java
@@ -5,8 +5,10 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ResponseMessage {
-    public static final String OK = "Request processed successfully";
-    public static final String CREATED = "Account created successfully";
-    public static final String EXPECTATION_FAILED = "Delete operation failed. Please try again or contact Dev team";
-    public static final String SERVER_ERROR = "An error occurred. Please try again or contact Dev team";
+    public static final String OK = "Request processed successfully.";
+    public static final String ACCOUNT_CREATED = "Account created successfully.";
+    public static final String ACCOUNT_TYPE_CREATED = "Account type created successfully.";
+    public static final String CUSTOMER_CREATED = "Customer created successfully.";
+    public static final String EXPECTATION_FAILED = "Delete operation failed.";
+    public static final String SERVER_ERROR = "Server error occurred.";
 }
