9dde4bfff562884a9b09c4b295248c98eb65bcf6
Askie01 <adam.paw01@gmail.com>
2024-09-20 17:32:58 +0200

    Update 'Response' & 'ErrorResponse'.
    Changed code type from 'String' to 'Integer.'

--- accounts/src/main/java/org/askie01/accounts/response/ErrorResponse.java
+++ accounts/src/main/java/org/askie01/accounts/response/ErrorResponse.java
@@ -2,7 +2,6 @@ package org.askie01.accounts.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
@@ -10,7 +9,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class ErrorResponse {
     private String path;
-    private HttpStatus code;
+    private Integer code;
     private String message;
     private LocalDateTime timestamp;
 }
