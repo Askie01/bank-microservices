0d820d5f479d3ca69c240552c1af7ce38ea0313c
Askie01 <adam.paw01@gmail.com>
2024-10-22 23:32:02 +0200

    Specify contract for 'LoanType' entity.

---
+++ loans/src/main/java/com/askie01/loans/entity/loan/type/LoanTypeEntity.java
@@ -0,0 +1,7 @@
+package com.askie01.loans.entity.loan.type;
+
+import com.askie01.loans.entity.abstraction.common.AuditableNamedEntity;
+
+public interface LoanTypeEntity<ID, Name, Time, User>
+        extends AuditableNamedEntity<ID, Name, Time, User> {
+}
